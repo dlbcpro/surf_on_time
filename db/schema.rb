3859ae916aca8a50c4d8a2ef19255ea07d1803c8
David Levy <dlbcpro@gmail.com>
2020-08-27 15:05:30 +0200

    migration-surf-wind-direction-forecast

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_123549) do
+ActiveRecord::Schema.define(version: 2020_08_27_130241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_123549) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "max_wave_height"
+    t.string "surf_wind_direction"
     t.index ["spot_id"], name: "index_forecasts_on_spot_id"
   end
 
