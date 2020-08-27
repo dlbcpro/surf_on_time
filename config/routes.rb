b6038f21659558215e1b21520381984f23f586d8
adelaide laanatri <laanatri.ade@gmail.com>
2020-08-27 11:23:57 +0200

    pagesspot

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spots, only: [:index, :show]
 
-  resources :schools, only: [:index, :show]
+  resources :surf_schools, only: [:index, :show]
 end
