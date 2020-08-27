61cab95e69d8838c9ea6af3168cf86e73819c876
David Levy <dlbcpro@gmail.com>
2020-08-27 14:38:24 +0200

    migration msw_id to spots

---
+++ app/services/msw.rb
@@ -0,0 +1,19 @@
+# app/services/msw.rb
+
+# class Msw
+#   def initialize(spot)
+#     @spot = spot
+#   end
+#   def create_forecasts
+#     # data = JSON.parse(File.open('toto.json'))
+#     data.each do |toto|
+#       Forecast.create(
+#         spot: @spot
+#       )
+#     end
+#   end
+# end
+# hossegor = Spot.first
+# Msw.new(hossegor).create_forecasts
+
+# File.open("#{Rails.root}/db/toto.json")
