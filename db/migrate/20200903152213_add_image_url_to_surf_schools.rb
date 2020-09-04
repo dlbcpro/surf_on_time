8662ca5f46f27e986c9516426268ca0f9d66e063
adelaide laanatri <laanatri.ade@gmail.com>
2020-09-04 06:43:03 +0200

    check

--- db/migrate/20200903152213_add_image_url_to_surf_schools.rb
+++ db/migrate/20200903152213_add_image_url_to_surf_schools.rb
@@ -1,5 +1,5 @@
 class AddImageUrlToSurfSchools < ActiveRecord::Migration[6.0]
   def change
-    add_column :surfschools, :image_url, :string
+    add_column :surf_schools, :image_url, :string
   end
 end
