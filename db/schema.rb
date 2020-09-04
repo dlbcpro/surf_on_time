a5f628a5548a663a8589c725e0b2835109996909
David Levy <dlbcpro@gmail.com>
2020-09-04 09:22:58 +0200

    add column image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_121405) do
+ActiveRecord::Schema.define(version: 2020_09_03_152221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_121405) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "msw_id"
+    t.string "image_url"
   end
 
   create_table "surf_schools", force: :cascade do |t|
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_121405) do
     t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
